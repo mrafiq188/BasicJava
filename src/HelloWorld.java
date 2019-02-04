6218b43615bf98f00c45c332aa936cbe3bb28382
rmunsur188 <munsur188@gmail.com>
2019-02-04 16:17:52 -0500

    java

---
+++ src/HelloWorld.java
@@ -0,0 +1,16 @@
+
+public class HelloWorld {
+
+	private static int i;
+
+	public static void main(String[] args) {
+
+		System.out.println(" MY Brain Harts ^_^");
+
+		int myInt = 2;
+		myInt = myInt+3+3;
+		System.out.println(myInt);
+		
+	}
+
+}
